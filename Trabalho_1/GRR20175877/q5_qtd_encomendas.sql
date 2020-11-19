3ddb8ec101e8d9755a2c37fe3a375b33fcb9db17
Douglas Affonso Clementino <douglas.affc@gmail.com>
2020-11-19 18:27:39 -0300

    Finalizando Trabalho 1 BD

---
+++ Trabalho_1/GRR20175877/q5_qtd_encomendas.sql
@@ -0,0 +1,1 @@
+select distinct(N_NAME), count(O_ORDERKEY) contador FROM CUSTOMER left outer join NATION on N_NATIONKEY = C_NATIONKEY left outer join ORDERS on C_CUSTKEY = O_CUSTKEY group by N_NAME order by contador desc;
---
+++ Trabalho_1/GRR20175877/q6_qtd_itens.sql
@@ -0,0 +1,1 @@
+select distinct(N_NAME), sum(L_QUANTITY) contador FROM CUSTOMER left outer join NATION on N_NATIONKEY = C_NATIONKEY left outer join ORDERS on C_CUSTKEY = O_CUSTKEY left outer join LINEITEM on O_ORDERKEY = L_ORDERKEY group by N_NAME order by contador desc;
